2d66f8537170abee64b940b7f8bcb0a93183038a
nkosinathintuli <nkosinathintuli21@gmail.com>
2020-10-20 14:46:14 +0200

    added takeVid and img

---
+++ picammodapipkg/takeImg.py
@@ -0,0 +1,16 @@
+from time import sleep
+from picamera import PiCamera
+import datetime 
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+# Camera warm-up time
+e = datetime.datetime.now()
+sleep(2)
+
+currentDateTime =  '_'+str(e.year)+str(e.month)+str(e.day)+'_'+str(e.hour)+str(e.minute)+str(e.second)
+filename = 'img'+currentDateTime
+camera.capture('/home/pi/picammodapipkg/picammodapipkg/imgOutput/'+filename+'.jpg')
+camera.stop_preview()
+
