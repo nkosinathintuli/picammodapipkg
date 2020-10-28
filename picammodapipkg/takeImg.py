ea4eedec8cc8356c889d69ec0765d77532902832
nydtab001 <53115370+nydtab001@users.noreply.github.com>
2020-10-28 15:57:27 +0200

    code cleanup

--- picammodapipkg/takeImg.py
+++ picammodapipkg/takeImg.py
@@ -1,16 +1,19 @@
+"""This module contains the function for taking a 1024x768 image.
+This module is imported by the motion_detect.py module
+
+"""
 from time import sleep
 from picamera import PiCamera
 import datetime 
 
 def takeImg():
-
+    """Captures a 1024x768 image and saves it on storage"""
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
-    # Camera warm-up time
+    # This is the camera warm-up time
     e = datetime.datetime.now()
     sleep(5)
-
     currentDateTime =  '_'+str(e.year)+str(e.month)+str(e.day)+'_'+str(e.hour)+str(e.minute)+str(e.second)
     filename = 'img'+currentDateTime
     camera.capture('/home/pi/Desktop/'+filename+'.jpg')
